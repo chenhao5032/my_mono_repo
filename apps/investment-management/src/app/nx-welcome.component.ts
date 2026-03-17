3f0b3caaee5fac56c56a5c81b2b82dc3ba35bc15
micky chan <1120103097@qq.com>
2026-03-17 17:39:53 +0800

    upgrade

--- apps/investment-management/src/app/nx-welcome.component.ts
+++ apps/investment-management/src/app/nx-welcome.component.ts
@@ -1,10 +1,10 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'my-mono-repo-nx-welcome',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   template: `
     <!--
      * * * * * * * * * * * * * * * * * * * * * * * * * * * *
